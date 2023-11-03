902f3a6b57f274eb8edb9ad2083bb00a0489ed3d
Alexia Tobares <avtobares@gmail.com>
2023-11-03 17:04:12 -0300

    testing 1 final

--- testing.js
+++ testing.js
@@ -1,5 +1,6 @@
 class ProductManager{
     array1 = []
+    id = 0
     constructor(){}
     getProducts(){
         console.log(this.array1)
@@ -9,7 +10,7 @@ class ProductManager{
         if (previousProduct){
             console.log('Error! No puedes repetir el codigo')
         }else{
-            this.array1.push({id:Math.random(), title, description, price, thumbnail, code, stock})
+            this.array1.push({id:this.id++, title, description, price, thumbnail, code, stock})
         }
         
 
@@ -17,7 +18,8 @@ class ProductManager{
     getProductById(id){
         const searchedProduct = this.array1.find((element)=> element.id === id)
         if(searchedProduct){
-            return searchedProduct
+            console.log('Producto encontrado:')
+            console.log(searchedProduct)
         }else{
             console.log('Error! No se ha encontrado el producto')
         }
@@ -29,3 +31,4 @@ product.getProducts()
 product.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25)
 product.getProducts()
 product.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25)
+product.getProductById(8)
